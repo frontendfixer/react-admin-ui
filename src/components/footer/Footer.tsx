3521c594d02dafee0f8957f54974513eceea03c9
Lakshmikanta <frontendfixer@gmail.com>
2023-07-18 07:28:24 +0530

    feat: create footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <span>store</span>
+      <span>@copyright reserved 2023</span>
+    </div>
+  );
+}
+
+export default Footer;
