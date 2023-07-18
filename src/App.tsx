fa6bcb449e67db4abb4bee4febcf4e401d5478ba
Lakshmikanta <frontendfixer@gmail.com>
2023-07-18 07:31:48 +0530

    feat: app page with custom layout and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import './styles/global.scss';
+
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+
+import Footer from './components/footer/Footer';
+import Menu from './components/menu/Menu';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Users from './pages/users/Users';
+import Product from './products/Products';
+
+function Layout() {
+  return (
+    <main className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/user',
+        element: <Users />,
+      },
+      {
+        path: '/product',
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello World</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
